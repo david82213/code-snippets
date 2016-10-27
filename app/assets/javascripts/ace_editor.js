5d7392bd6f4a730792b79e1825d70f7d3aafd6dd
Song Ji <songji0509@gmail.com>
2016-10-27 15:48:19 -0700

    Added read only textarea in ace-editor.js, and solved problem editor not reloading

--- app/assets/javascripts/ace_editor.js
+++ app/assets/javascripts/ace_editor.js
@@ -6,6 +6,7 @@
 //   textarea.val(editor.getSession().getValue());
 // });
 
+// This turns div .source-editor to editable editor 
 $('.source-editor').each(function() {
   var container = $(this);
   var mode = container.data('mode');
@@ -26,6 +27,36 @@ $('.source-editor').each(function() {
   editor.setFontSize('14px');
   editor.setTheme('ace/theme/monokai');
   // editor.setReadOnly(true);// this makes the textarea display only
+  editor.getSession().setMode('ace/mode/javascript');
+  editor.getSession().setValue(editorArea.val());
+
+  // save back to the textarea when submit
+  editorArea.closest('form').submit(function() {
+    editorArea.val(editor.getSession().getValue());
+  });
+});
+
+// This turns div with readonly-editor tag to readonly editor
+$('.readonly-editor').each(function() {
+  var container = $(this);
+  var mode = container.data('mode');
+  var editorArea = container.find('textarea');
+
+  // build a edit div for ACE since ACE can't load in a textarea
+  var editDiv = $('<div>', {
+    position: 'absolute',
+      width: editorArea.width(),
+      height: editorArea.height(),
+      'class': editorArea.attr('class')
+    }).insertBefore(editorArea);
+  // no need to display textarea
+  editorArea.css('display', 'none');
+
+  var editor = ace.edit(editDiv[0]);
+
+  editor.setFontSize('14px');
+  editor.setTheme('ace/theme/monokai');
+  editor.setReadOnly(true);// this makes the textarea display only
   editor.getSession().setMode('ace/mode/javascript');
   editor.getSession().setValue(editorArea.val());
 
