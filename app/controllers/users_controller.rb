04daa6139d8a5c46687d12c3024127b91aa722d5
Daniel Lin <david82213@hotmail.com>
2016-10-29 11:30:04 -0700

    Implemented user registeration and UI

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    user_params = params.require(:user).permit([:first_name,
+                                                :last_name,
+                                                :email,
+                                                :password,
+                                                :password_confirmation])
+
+    @user = User.new user_params
+    if @user.save
+      redirect_to codes_path, notice: 'Thanks for register'
+    else
+      render :new
+    end
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: VALID_EMAIL_REGEX
+end
