9de14361a09cda34a9a4b469ae591e9dcd18cb59
Song Ji <songji0509@gmail.com>
2016-10-30 22:15:59 -0700

    Add one more testing for language model

--- spec/models/language_spec.rb
+++ spec/models/language_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Language, type: :model do
   describe "validation" do
+    it 'requires a language name' do
+      l = build(:language, name: nil)
+      l.valid?
+      expect(l.errors).to have_key(:name)
+    end
+    
     it 'requires a unique language name' do
       create(:language, name: 'Ruby')
       l = build(:language, name: 'Ruby')
