d200a545d0fbd223309e24d58b1945573d0c28e5
Song Ji <songji0509@gmail.com>
2016-11-01 18:31:33 -0700

    Add rspec for codes controller

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -28,7 +28,7 @@ class Ability
     #
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
-    user = User.new
+    user ||= User.new
 
     can :manage, Code do |c|
       c.user == user
